7f8ab98a7a26a412f67ac8920c07b7250c42210b
Hiura <hiura0012@gmail.com>
2026-05-01 21:20:54 +0700

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,10 +3,9 @@ module.exports = function (api) {
   return {
     presets: [
       ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
-      'nativewind/babel',
     ],
     plugins: [
-      'react-native-reanimated/plugin', // ← ini yang kurang!
+      'react-native-reanimated/plugin',
     ],
   };
 };
