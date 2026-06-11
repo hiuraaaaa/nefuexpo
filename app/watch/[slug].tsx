14b18d472b24f9f42a115ff1cafd269e4d6755e8
Hiura <hiura0012@gmail.com>
2026-06-11 22:08:40 +0700

    Update [slug].tsx

--- app/watch/[slug].tsx
+++ app/watch/[slug].tsx
@@ -21,7 +21,7 @@ import Animated, {
 } from 'react-native-reanimated';
 import { useBatteryLevel, useBatteryState, BatteryState } from 'expo-battery';
 import { COLORS } from '@/constants';
-import { api, getAnimeSlug, decodeAnimeId, formatTime } from '@/hooks/api';
+import { api, getAnimeSlug, decodeAnimeId, formatTime } from '@/hooks/api/api';
 import { historyStorage, progressStorage, favoritStorage, storageMain } from '@/hooks/storage';
 import { xpStorage } from '@/hooks/xp';
 import { AnimeDetail, Episode, Server, Anime } from '@/types';
